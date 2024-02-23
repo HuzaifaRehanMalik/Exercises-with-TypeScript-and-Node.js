bacca5195f564116b25cef489c7d605b63fb0ffc
Huzaifa <157941777+HuzaifaRehanMalik@users.noreply.github.com>
2024-02-23 19:34:40 +0500

    Add files via upload

---
+++ Q5.js
@@ -0,0 +1,3 @@
+var famous_person = 'Nelson Mandela famously said, "Education is the most powerful weapon which you can use to change the world."';
+var massage = famous_person;
+console.log(massage);
---
+++ Q5.ts
@@ -0,0 +1,5 @@
+let famous_person:string='Nelson Mandela famously said, "Education is the most powerful weapon which you can use to change the world."'
+let massage:string=famous_person
+console.log(massage);
+
+

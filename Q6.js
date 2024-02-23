944cadac0a54a587ab56b121649aa0bddbdf8527
Huzaifa <157941777+HuzaifaRehanMalik@users.noreply.github.com>
2024-02-23 19:47:53 +0500

    Add files via upload

---
+++ Q6.js
@@ -0,0 +1,4 @@
+var person_name = "\t\n Huzaifa \n\t";
+console.log("Name with whitespace:", person_name);
+var strripName = person_name.trim();
+console.log("Stripped name:", strripName);
---
+++ Q6.ts
@@ -0,0 +1,5 @@
+let person_name:string="\t\n Huzaifa \n\t"
+console.log("Name with whitespace:",person_name);
+
+let strripName:string=person_name.trim();
+console.log("Stripped name:",strripName);

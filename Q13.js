2f49da9a520e99559697253eb4ce44717f0d8d76
Huzaifa <157941777+HuzaifaRehanMalik@users.noreply.github.com>
2024-03-01 09:49:14 +0500

    Add files via upload

---
+++ Q13.js
@@ -0,0 +1,5 @@
+var favoriteCars = ["Tesla Model S", "Porsche 911", "BMW M3", "Audi R8", "Ford Mustang"];
+for (var _i = 0, favoriteCars_1 = favoriteCars; _i < favoriteCars_1.length; _i++) {
+    var car = favoriteCars_1[_i];
+    console.log("my favorite car name is ".concat(car, "!"));
+}
---
+++ Q13.ts
@@ -0,0 +1,7 @@
+// Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+
+
+let favoriteCars: string[] = ["Tesla Model S", "Porsche 911", "BMW M3", "Audi R8", "Ford Mustang"];
+for (let car of favoriteCars){
+console.log(`my favorite car name is ${car}!`);
+}

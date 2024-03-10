9d56d09ff837f56213d86d714cee32b128634114
Huzaifa <157941777+HuzaifaRehanMalik@users.noreply.github.com>
2024-03-10 11:13:22 +0500

    Add files via upload

---
+++ Q27.js
@@ -0,0 +1,17 @@
+// Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-else chain.
+// • If the alien is green, print a message that the player earned 5 points.
+// • If the alien is yellow, print a message that the player earned 10 points.
+// • If the alien is red, print a message that the player earned 15 point
+var alien_color = "green";
+if (alien_color == "green") {
+    console.log("You just earned 5 points");
+}
+else if (alien_color == "red") {
+    console.log("You just earned 10 points");
+}
+else if (alien_color == "yellow") {
+    console.log("You just earned 15 points");
+}
+else {
+    console.log("You just earned 20 points");
+}

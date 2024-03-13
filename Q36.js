78498ab22f15113c19d9acc04cf321bf6ccab6b5
Huzaifa <157941777+HuzaifaRehanMalik@users.noreply.github.com>
2024-03-13 15:25:16 +0500

    Add files via upload

---
+++ Q36.js
@@ -0,0 +1,5 @@
+// T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. The function should print a sentence summarizing the size of the shirt and the message printed on it. Call the function.
+function make_shirt(size, message) {
+    console.log("make me a shirt size ".concat(size, " and print a message on it that ").concat(message));
+}
+make_shirt("small", "Code enthusiast");

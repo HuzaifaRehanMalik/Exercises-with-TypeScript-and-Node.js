6aa23009ea11fa8d565e4c9dae0d704797ee20de
Huzaifa <157941777+HuzaifaRehanMalik@users.noreply.github.com>
2024-03-14 15:17:06 +0500

    Add files via upload

---
+++ Q39.js
@@ -0,0 +1,9 @@
+// City Names: Write a function called city_country() that takes in the name of a city and its country. The function should return a string formatted like this:
+// "Lahore, Pakistan"
+// Call your function with at least three city-country pairs, and print the value that’s returned.
+function Describing_cities(city, country) {
+    console.log("".concat(city, " is a city of ").concat(country));
+}
+Describing_cities("Jerusalem", "Palestine");
+Describing_cities("Lahore", "Pakistan");
+Describing_cities("Ankara", "Turkey");
